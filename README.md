b60890cfd46cdad9487c44ef98ff879dee600fab
Erik Zscheile <erik.zscheile@gmail.com>
2019-05-20 22:53:35 +0200

    import files from zsconfuz repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# zsconfuz-alt
+
+A parallel C++ 'configure' program.
+This program simulates [zsconfuz](https://github.com/zserik/zsconfuz) using ninja.
+
+NOTE: this utility only generates
+  an unix shell file with the variable definitions
+
+## Dependencies
+
+ - C++17 compiler
+ - CMake
+ - Ninja
+ - bash
---
+++ src/ppsec.cxx
@@ -0,0 +1,19 @@
+/** zsconfuz ppsec.cxx
+    (C) 2019 Erik Zscheile
+    License: ISC
+ **/
+#include <stdio.h>
+#include <string>
+using std::string;
+
+int main(int argc, char *argv[]) {
+  if(argc == 1) {
+    fprintf(stderr, "USAGE: zsconfuz-ppsec SECTION_NAME\n");
+    return 1;
+  }
+  puts("");
+  const string secname = argv[1];
+  const string sepad(secname.size() + 6, '-');
+  printf("%s\n   \033[1m%s\033[0m\n%s\n", sepad.c_str(), secname.c_str(), sepad.c_str());
+  return 0;
+}
